fb23870f45f59426e581e71fdf23fe157fe12320
Zane <1015767@student.sae.edu.au>
2019-08-21 12:57:45 +1000

    Initial commit

---
+++ Assets/Low Poly Western Saloon/Prefabs/example.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class example : MonoBehaviour
+{
+    [Range(0, 100)]
+    public float Health;
+    public GameObject P1HP;
+    Animator P1BAR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        P1BAR = P1HP.GetComponent<Animator>();
+        Health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1BAR.SetFloat("Health", Health);
+    }
+}
